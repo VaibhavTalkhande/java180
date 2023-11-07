ebb04095fbc5b1f8692969cb797e8f65eb9587c8
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-11-07 15:47:12 +0530

    inputcheck

--- src/InputType.java
+++ src/InputType.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class InputType {
+import java.util.Scanner;
+
+public class InputType {
+    public static void main(String[] args){
+
+        System.out.print("enter a value to check type");
+        Scanner sc = new Scanner(System.in);
+//        int val1= sc.nextInt();
+        System.out.println(sc.hasNextInt());
+    }
 }
