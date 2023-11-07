73814210984a839fb91e3f43f8db96b8fd1cd605
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-11-07 15:13:48 +0530

    input&output

---
+++ src/AddThreeSum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AddThreeSum {
+    public static void main(String[] args){
+        Scanner input_value = new Scanner(System.in);
+        int answer=0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("Enter the "+(i+1)+"st  value:");
+            answer+= input_value.nextInt();
+
+        }
+        System.out.println("your answer:"+ answer);
+    }
+
+}
