5dd96fbae1389bb56f780cac69556960200e47b4
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2023-12-14 18:37:05 +0530

    FileHandling LibraryManagement

---
+++ src/FileHandling.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileHandling {
+    public static void main(String[] args) {
+
+        File myFile = new File("CWH_file1.txt");
+        try {
+            myFile.createNewFile();
+            System.out.println("File created successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Code to write to a file
+        try {
+            FileWriter fileWriter = new FileWriter("cwh111file.txt");
+            fileWriter.write("This is our first file from this java course\nOkay now bye");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Reading a file
+        try {
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // Deleting a file
+        if(myFile.delete()){
+            System.out.println("I have deleted: " + myFile.getName());
+        }
+        else{
+            System.out.println("Some problem occurred while deleting the file");
+        }
+
+    }
+}
